8d5ada129d2db70e9abab08c372f69016fd24edf
Santiago G. Alegria <santiago.giraldo.alegria@gmail.com>
2024-10-15 02:45:16 -0500

    wip: update tune command

--- earthquakes/lstm/commands.py
+++ earthquakes/lstm/commands.py
@@ -1,115 +1,131 @@
 import logging
 import os
 from pathlib import Path
+import pickle
 
 import click
-import pandas as pd
 import ray
-from data.data import EarthquakeData
-from dotenv import load_dotenv
-from lstm.regression import LSTMTrainable, test_result
 from ray import tune
 from ray.tune import ExperimentAnalysis, ResultGrid
 from ray.tune.schedulers import AsyncHyperBandScheduler as ASHAScheduler
-from settings import read_coordinates
+
+from data.data import EarthquakeData
+from data.usgs import USGS
+from data.grid import Grid
+from lstm import utils
+from lstm.regression import RegressionTrainable, test_result
 
 logger = logging.getLogger(__name__)
 
 
-def split_n_parse(string: str, _type: type):
-    return [_type(part) for part in string.split(",") if part]
-
-
-def prompt_experiment():
-    ray_results = Path.home() / "ray_results"
-    folders = {
-        idx: folder
-        for idx, folder in enumerate(
-            ray_results.glob("*"),
-        )
-        if folder.is_dir()
-        if folder.stem[0].isalpha()
-    }
-    prompt = "\n".join(f"{idx}) {folder.stem}" for idx, folder in folders.items())
-    choice = click.prompt(prompt, type=int, default=None)
-    assert choice is not None, choice
-    return folders.get(choice)
-
-
-def load_data(file: str, env: str) -> EarthquakeData:
-    load_dotenv(env, override=True)
-    file_path = file or os.getenv("FILE_PATH")
-    assert file_path, "FILE_PATH was not provided"
-    raw_data = pd.read_csv(file_path)
-    latitude, longitude = read_coordinates()
-    return EarthquakeData(
-        raw_data,
-        ["latitude", "longitude", "depth", "mag"],
-        ["mag"],
-        zero_columns=["depth", "mag"],  # can be zero when scaling
-        time_column=True,
-        delta_time=True,
-        drop_time_column=True,
-        min_latitude=min(latitude),
-        min_longitude=min(longitude),
-        min_magnitude=0,
-        max_magnitude=9,
-    )
-
-
 @click.command(name="tune")
-@click.option("-f", "--file", type=str, help="csv earthquake catalog to be processed")
-@click.option("-e", "--env", type=str, help="env")
+@click.option(
+    "--features",
+    multiple=True,
+    help="features to be used in the model",
+    default=["latitude", "longitude", "depth", "mag"],
+    callback=lambda ctx, param, value: list(value),
+)
+@click.option("--target", type=str, help="target to be predicted", default="mag")
+@click.option("--min-lat", type=float, help="min latitude", default=-0.132)
+@click.option("--max-lat", type=float, help="max latitude", default=9.796)
+@click.option("--min-long", type=float, help="min longitude", default=-80.343)
+@click.option("--max-long", type=float, help="max longitude", default=-72.466)
+@click.option("--min-mag", type=float, help="min magnitude", default=0)
+@click.option("--max-mag", type=float, help="max magnitude", default=10)
+@click.option("--node-size", type=int, help="size of node in kms", default=100)
+@click.option("--metric", type=str, help="metric", default="loss")
+@click.option("--mode", type=str, help="mode", default="min")
+@click.option("--networkx", type=bool, help="mode", default=False)
+@click.option(
+    "--nx-features",
+    multiple=True,
+    help="features to be used in the model",
+    default=["degree_centrality", "clustering", "betweenness_centrality", "closeness_centrality", "pagerank"],
+    callback=lambda ctx, param, value: list(value),
+)
+@click.option("--nx-lookback", type=int, help="mode", default=5)
 @click.option("-s", "--samples", type=int, help="samples", default=-1)
-def tune_command(file, env, samples):
+def tune_regression(
+    features,
+    target,
+    min_lat,
+    max_lat,
+    min_long,
+    max_long,
+    min_mag,
+    max_mag,
+    node_size,
+    metric,
+    mode,
+    networkx,
+    nx_features,
+    nx_lookback,
+    samples,
+):
     """Reads a processed .csv catalog and trains an LSTM neural network"""
-    qdata = load_data(file, env)
+    logger.info("Downloading data...")
+    latitude = (min_lat, max_lat)
+    longitude = (min_long, max_long)
+    raw_data = USGS(latitude, longitude).download()
+    kwargs = {}
+    if networkx:
+        grid = Grid(latitude, longitude, node_size)
+        kwargs["grid"] = grid
+        kwargs["network_features"] = nx_features
+        kwargs["network_lookback"] = nx_lookback
+
+    qdata = EarthquakeData(raw_data, features, [target], min_magnitude=min_mag, max_magnitude=max_mag, **kwargs)
+
     logger.info("Processing data...")
-    metric = os.getenv("METRIC", "loss")
-    opt_mode = "min"
-    logger.info("Tuning with metric %s mode: %s", metric, opt_mode)
-    scheduler = ASHAScheduler(metric=metric, mode=opt_mode, grace_period=1, reduction_factor=2)
-    trainable = tune.with_parameters(LSTMTrainable, qdata=qdata)
+    utils.plot_analysis(qdata.data, features, target, Path.home() / "plots" / qdata.hash)
+
+    logger.info("Tuning with metric %s mode: %s", metric, mode)
+    scheduler = ASHAScheduler(metric=metric, mode=mode, grace_period=1, reduction_factor=2)
+    trainable = tune.with_parameters(RegressionTrainable, qdata=qdata)
 
     ray.init(dashboard_host="0.0.0.0", ignore_reinit_error=True)
     tuner = tune.Tuner(
         tune.with_resources(trainable, resources={"cpu": 8, "gpu": 1}),
-        tune_config=tune.TuneConfig(
-            scheduler=scheduler,
-            num_samples=samples,
-            max_concurrent_trials=None,
-        ),
+        tune_config=tune.TuneConfig(scheduler=scheduler, num_samples=samples, max_concurrent_trials=None),
         param_space={
             "lookback": tune.randint(10, 150),
             "test_size": tune.uniform(0.1, 0.3),
             "batch_size": tune.randint(2, 20),
-            "hidden_size": tune.randint(32, 128),
+            "hidden_size": tune.randint(10, 150),
             "lstm_layers": tune.randint(2, 10),
             "lr": tune.loguniform(1e-4, 1e-2),
-            "max_epochs": tune.randint(10, 50),
+            "max_epochs": tune.randint(10, 70),
             "loss_type": tune.choice(["mse", "mape", "mae"]),
         },
     )
     results = tuner.fit()
     logger.info("Results path at %s", results.experiment_path)
-    best_result = results.get_best_result(metric, opt_mode)
-    test_result(best_result, qdata)
+    best_result = results.get_best_result(metric, mode)
+    test_result(best_result, qdata, metric, mode)
+    # pickle qdata
+    with open(Path(results.experiment_path) / "qdata.pkl", "wb") as f:
+        pickle.dump(qdata, f)
 
 
 @click.command(name="test")
 @click.option("-ex", "--experiment-path", type=str, help="experiment path", default=None)
-@click.option("-e", "--env", type=str, help="env path", default=None)
-def test_command(experiment_path, env):
-    result_path = Path(experiment_path).resolve() if experiment_path else prompt_experiment()
+@click.option("--metric", type=str, help="metric", default="loss")
+@click.option("--mode", type=str, help="mode", default="min")
+def test_command(experiment_path, metric, mode):
+    result_path = Path(experiment_path).resolve() if experiment_path else utils.prompt_experiment()
+
+    logger.info("Loading qdata...")
+    with open(Path(result_path) / "qdata.pkl", "rb") as f:
+        qdata = pickle.load(f)
+
     logger.info("Loading %s", result_path)
     analysis = ExperimentAnalysis(result_path)
     result_grid = ResultGrid(analysis)
-    metric = os.getenv("METRIC", "loss")
-    result = result_grid.get_best_result(metric, "min")
-    qdata = load_data(None, env)
-    test_result(result, qdata)
+    result = result_grid.get_best_result(metric, mode)
+    test_result(result, qdata, metric, mode)
 
 
 lstm_group = click.Group("lstm", help="tools to train and tune lstm models")
-lstm_group.add_command(tune_command)
+lstm_group.add_command(tune_regression)
 lstm_group.add_command(test_command)
