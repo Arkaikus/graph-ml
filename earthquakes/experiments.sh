cdedfe7407229ca9b47f1f9689cac3dda59aad72
Santiago G. Alegria <santiago.giraldo.alegria@gmail.com>
2024-10-17 11:17:48 -0500

    wip experiments

---
+++ earthquakes/experiments.sh
@@ -0,0 +1,19 @@
+# quakes lstm tune --samples 10
+
+quakes lstm tune --samples 10 --netwrokx t --nx-feature degree_centrality --node-size 50
+quakes lstm tune --samples 10 --netwrokx t --nx-feature clustering --node-size 50
+quakes lstm tune --samples 10 --netwrokx t --nx-feature betweenness_centrality --node-size 50
+quakes lstm tune --samples 10 --netwrokx t --nx-feature closeness_centrality --node-size 50
+quakes lstm tune --samples 10 --netwrokx t --nx-feature pagerank --node-size 50
+
+quakes lstm tune --samples 10 --netwrokx t --nx-feature degree_centrality --node-size 100
+quakes lstm tune --samples 10 --netwrokx t --nx-feature clustering --node-size 100
+quakes lstm tune --samples 10 --netwrokx t --nx-feature betweenness_centrality --node-size 100
+quakes lstm tune --samples 10 --netwrokx t --nx-feature closeness_centrality --node-size 100
+quakes lstm tune --samples 10 --netwrokx t --nx-feature pagerank --node-size 100
+
+quakes lstm tune --samples 10 --netwrokx t --nx-feature degree_centrality --node-size 150
+quakes lstm tune --samples 10 --netwrokx t --nx-feature clustering --node-size 150
+quakes lstm tune --samples 10 --netwrokx t --nx-feature betweenness_centrality --node-size 150
+quakes lstm tune --samples 10 --netwrokx t --nx-feature closeness_centrality --node-size 150
+quakes lstm tune --samples 10 --netwrokx t --nx-feature pagerank --node-size 150
