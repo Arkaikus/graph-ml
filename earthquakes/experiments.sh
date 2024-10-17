986aa741e40319c5578d84bfa83f976bb237d972
Santiago G. Alegria <santiago.giraldo.alegria@gmail.com>
2024-10-17 11:21:46 -0500

    fix experiments script

--- earthquakes/experiments.sh
+++ earthquakes/experiments.sh
@@ -1,19 +1,19 @@
 # quakes lstm tune --samples 10
 
-quakes lstm tune --samples 10 --networkx t --nx-feature degree_centrality --node-size 50
-quakes lstm tune --samples 10 --networkx t --nx-feature clustering --node-size 50
-quakes lstm tune --samples 10 --networkx t --nx-feature betweenness_centrality --node-size 50
-quakes lstm tune --samples 10 --networkx t --nx-feature closeness_centrality --node-size 50
-quakes lstm tune --samples 10 --networkx t --nx-feature pagerank --node-size 50
+quakes lstm tune --samples 10 --networkx t --nx-features degree_centrality --node-size 50
+quakes lstm tune --samples 10 --networkx t --nx-features clustering --node-size 50
+quakes lstm tune --samples 10 --networkx t --nx-features betweenness_centrality --node-size 50
+quakes lstm tune --samples 10 --networkx t --nx-features closeness_centrality --node-size 50
+quakes lstm tune --samples 10 --networkx t --nx-features pagerank --node-size 50
 
-quakes lstm tune --samples 10 --networkx t --nx-feature degree_centrality --node-size 100
-quakes lstm tune --samples 10 --networkx t --nx-feature clustering --node-size 100
-quakes lstm tune --samples 10 --networkx t --nx-feature betweenness_centrality --node-size 100
-quakes lstm tune --samples 10 --networkx t --nx-feature closeness_centrality --node-size 100
-quakes lstm tune --samples 10 --networkx t --nx-feature pagerank --node-size 100
+quakes lstm tune --samples 10 --networkx t --nx-features degree_centrality --node-size 100
+quakes lstm tune --samples 10 --networkx t --nx-features clustering --node-size 100
+quakes lstm tune --samples 10 --networkx t --nx-features betweenness_centrality --node-size 100
+quakes lstm tune --samples 10 --networkx t --nx-features closeness_centrality --node-size 100
+quakes lstm tune --samples 10 --networkx t --nx-features pagerank --node-size 100
 
-quakes lstm tune --samples 10 --networkx t --nx-feature degree_centrality --node-size 150
-quakes lstm tune --samples 10 --networkx t --nx-feature clustering --node-size 150
-quakes lstm tune --samples 10 --networkx t --nx-feature betweenness_centrality --node-size 150
-quakes lstm tune --samples 10 --networkx t --nx-feature closeness_centrality --node-size 150
-quakes lstm tune --samples 10 --networkx t --nx-feature pagerank --node-size 150
+quakes lstm tune --samples 10 --networkx t --nx-features degree_centrality --node-size 150
+quakes lstm tune --samples 10 --networkx t --nx-features clustering --node-size 150
+quakes lstm tune --samples 10 --networkx t --nx-features betweenness_centrality --node-size 150
+quakes lstm tune --samples 10 --networkx t --nx-features closeness_centrality --node-size 150
+quakes lstm tune --samples 10 --networkx t --nx-features pagerank --node-size 150
